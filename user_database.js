ea273de96bc8dff0f4236a2b439f0fb8af4d2be1
Akpemie Darlington Alloh <xbladex16@gmail.com>
2024-06-30 09:29:06 +0100

    Update user_database.js

--- user_database.js
+++ user_database.js
@@ -3,5 +3,5 @@ const userDatabase = [];
 app.post('/submit', (req, res) => {
     const { name, email } = req.body;
     userDatabase.push({ name, email });
-    res.send('Data received successfully!');
+    res.send('Successful');
 });
